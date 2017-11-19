eb6d62bcf8234af28714b9931fefc2fe54370a12
Your Name <you@example.com>
2017-11-19 16:29:56 -0300

    Change library filename and fix ctypes import in loader

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 import ctypes
 
-c_library = ctypes.CDLL('./out.so')
+c_library = ctypes.CDLL('./library.so')
 
 c_library.sumAll.argtypes = (ctypes.c_int,)
 
@@ -8,4 +8,9 @@ def sumAll(x):
     global c_library
     return c_library.sumAll(
         ctypes.c_int(x)
+    )
+
+def sumAll_python(x):
+    return sum(
+        range(x+1)
     )
